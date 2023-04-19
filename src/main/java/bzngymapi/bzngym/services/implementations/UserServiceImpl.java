f06a34bd83656d6b3fc0ae7d205f30b8d362b2bf
SergiuScorbureanu <sergiu.scorbureanu@gmail.com>
2023-04-19 20:28:43 +0300

    Added verification for email and phone User fields

--- src/main/java/bzngymapi/bzngym/services/implementations/UserServiceImpl.java
+++ src/main/java/bzngymapi/bzngym/services/implementations/UserServiceImpl.java
@@ -6,6 +6,7 @@ import bzngymapi.bzngym.services.interfaces.IUserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 public class UserServiceImpl implements IUserService {
@@ -25,4 +26,17 @@ public class UserServiceImpl implements IUserService {
     public void createUser(User user) {
         userRepository.save(user);
     }
+
+    public static boolean isEmailValid (String emailAddress) {
+        return Pattern.compile("^(.+)@(\\S+)$")
+                .matcher(emailAddress)
+                .matches();
+    }
+
+    public static boolean isPhoneValid (String phoneNumber) {
+        return Pattern.compile("^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$")
+                .matcher(phoneNumber)
+                .matches();
+    }
+
 }
